510402c1e3887a3341b866cfc2e2bf93a2d3fbd3
RA_ONE <125670877+7thRA-ONE@users.noreply.github.com>
2023-10-02 16:21:50 +0530

    Update reactions.js

--- Plugins/reactions.js
+++ Plugins/reactions.js
@@ -1,4 +1,4 @@
-const {
+ const {
   fetchJson,
   getBuffer,
   GIFBufferToVideoBuffer,
@@ -28,7 +28,7 @@ let mergedCommands = [
   "slap",
   "smile",
   "smug",
-  "wave",
+  "wave", 
   "wink",
   "yeet",
 ];
@@ -66,79 +66,89 @@ module.exports = {
   ],
   description: "All reaction Commands",
   start: async (Atlas, m, { text, prefix, mentionByTag, doReact }) => {
-    const suitableWords = {
-      bite: "bited",
-      blush: "is blushing at",
-      bonk: "bonked",
-      bully: "is bullying",
-      cringe: "cringed at",
-      cry: "cried in front of",
-      cuddle: "is cuddling",
-      dance: "is dancing with",
-      glomp: "glomped at",
-      handhold: "holding hands of",
-      happy: "is happy with",
-      highfive: "high-fived at",
-      hug: "is hugging",
-      kick: "kicked",
-      kill: "killed",
-      kiss: "is kissing",
-      lick: "is licking",
-      nom: "is eating with",
-      pat: "is patting",
-      poke: "is poking",
-      slap: "slapped",
-      smile: "is smiling at",
-      smug: "smugged at",
-      wave: "waved at",
-      wink: "Winked at",
-      yeet: "Yeeted at",
-    };
-    await doReact("🎭");
+    try {
+      const suitableWords = {
+        bite: "bited",
+        blush: "is blushing at",
+        bonk: "bonked",
+        bully: "is bullying",
+        cringe: "cringed at",
+        cry: "cried in front of",
+        cuddle: "is cuddling",
+        dance: "is dancing with",
+        glomp: "glomped at",
+        handhold: "holding hands of",
+        happy: "is happy with",
+        highfive: "high-fived at",
+        hug: "is hugging",
+        kick: "kicked",
+        kill: "killed",
+        kiss: "is kissing",
+        lick: "is licking",
+        nom: "is eating with",
+        pat: "is patting",
+        poke: "is poking",
+        slap: "slapped",
+        smile: "is smiling at",
+        smug: "smugged at",
+        wave: "waved at",
+        wink: "Winked at",
+        yeet: "Yeeted at",
+      };
+      await doReact("🍁");
 
-    const reactions = Object.keys(suitableWords);
-    const command = m.body
-      .split(" ")[0]
-      .toLowerCase()
-      .slice(prefix.length)
-      .trim();
-    const capitalize = (content) =>
-      `${content.charAt(0).toUpperCase()}${content.slice(1)}`;
-    let flag = true;
-    if (command === "r" || command === "reaction") flag = false;
-    if (!flag && !text) {
-      const reactionList = `🎃 *Available Reactions:*\n\n- ${reactions
-        .map((reaction) => capitalize(reaction))
-        .join(
-          "\n- "
-        )}\n🛠️ *Usage:* ${prefix}reaction (reaction) [tag/quote user] | ${prefix}(reaction) [tag/quote user]\nExample: ${prefix}pat`;
-      return void (await m.reply(reactionList));
-    }
-    const reaction = flag ? command : text.split(" ")[0].trim().toLowerCase();
-    if (!flag && !reactions.includes(reaction))
-      return void m.reply(
-        `Invalid reaction. Use *${prefix}react* to see all of the available reactions`
+      const reactions = Object.keys(suitableWords);
+      const command = m.body
+        .split(" ")[0]
+        .toLowerCase()
+        .slice(prefix.length)
+        .trim();
+      const capitalize = (content) =>
+        `${content.charAt(0).toUpperCase()}${content.slice(1)}`;
+      let flag = true;
+      if (command === "r" || command === "reaction") flag = false;
+      if (!flag && !text) {
+        const reactionList = `🎃 *Available Reactions:*\n\n- ${reactions
+          .map((reaction) => capitalize(reaction))
+          .join(
+            "\n- "
+          )}\n🛠️ *Usage:* ${prefix}reaction (reaction) [tag/quote user] | ${prefix}(reaction) [tag/quote user]\nExample: ${prefix}pat`;
+        return void (await m.reply(reactionList));
+      }
+      const reaction = flag
+        ? command
+        : text.split(" ")[0].trim().toLowerCase();
+      if (!flag && !reactions.includes(reaction))
+        return void m.reply(
+          `Invalid reaction. Use *${prefix}react* to see all of the available reactions`
+        );
+      const users = mentionByTag;
+      if (m.quoted && !users.includes(m.quoted.sender))
+        users.push(m.quoted.sender);
+      while (users.length < 1) users.push(m.sender);
+      const reactant = users[0];
+      const single = reactant === m.sender;
+      const { url } = await fetchJson(`https://api.waifu.pics/sfw/${reaction}`);
+      const result = await getBuffer(url);
+      const buffer = await GIFBufferToVideoBuffer(Buffer.from(result, "utf-8"));
+      const userResponse = single ? "Themselves" : `@${reactant.split("@")[0]}`;
+      const txtResponse = suitableWords[reaction];
+      const caption = `*@${m.sender.split("@")[0]} ${txtResponse} ${userResponse} like a pro!*`;
+
+      await Atlas.sendMessage(
+        m.from,
+        {
+          video: buffer,
+          gifPlayback: true,
+          caption: caption,
+          mentions: [m.sender, reactant],
+        },
+        { quoted: m }
       );
-    const users = mentionByTag;
-    if (m.quoted && !users.includes(m.quoted.sender))
-      users.push(m.quoted.sender);
-    while (users.length < 1) users.push(m.sender);
-    const reactant = users[0];
-    const single = reactant === m.sender;
-    const { url } = await fetchJson(`https://api.waifu.pics/sfw/${reaction}`);
-    const result = await getBuffer(url);
-    const buffer = await GIFBufferToVideoBuffer(Buffer.from(result, "utf-8"));
-    await Atlas.sendMessage(
-      m.from,
-      {
-        video: buffer,
-        gifPlayback: true,
-        caption: `*@${m.sender.split("@")[0]} ${suitableWords[reaction]} ${
-          single ? "Themselves" : `@${reactant.split("@")[0]}`
-        }*`,
-        mentions: [m.sender, reactant],
-      },
-      { quoted: m }
-    );
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle the error and send an appropriate response
+      await m.reply("An error occurred while processing your request.");
+    }
   },
 };
